0cca18abe6b93396433872150aaffa44f0a8629a
Prince <princekumaraman7654@gmail.com>
2025-05-25 00:37:03 +0530

    create a base model and extend the child classes

--- src/main/java/org/example/models/Review.java
+++ src/main/java/org/example/models/Review.java
@@ -2,43 +2,23 @@ package org.example.models;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.util.Date;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "bookingReview") //custom name for the table
-public class Review {
-
-    @Id  // makes the field as primary key in our DB table
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB auto increments the ID
-    private Long id;
+public class Review extends baseModel{
 
     @Column(nullable = false)
     private String Content;
 
     private double rating;
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP) // decides the format of date stored
-    @CreatedDate // tells the spring to add value when created
-    private Date createdAt;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate // tells the spring to add value when updated
-    private Date updatedAt;
-
     @Override
     public String toString(){
-        return "Review: " + this.Content + " " + this.rating+ " " + this.createdAt;
+        return "Review: " + this.Content + " " + this.rating + " " + this.createdAt;
     }
 }
---
+++ src/main/java/org/example/models/baseModel.java
@@ -0,0 +1,30 @@
+package org.example.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass //handles inheritance in Spring data JPA
+@Getter
+@Setter
+public abstract class baseModel {
+    @Id  // makes the field as primary key in our DB table
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB auto increments the ID
+    protected Long id;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // decides the format of date stored
+    @CreatedDate // tells the spring to add value when created
+    protected Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate // tells the spring to add value when updated
+    protected Date updatedAt;
+}
