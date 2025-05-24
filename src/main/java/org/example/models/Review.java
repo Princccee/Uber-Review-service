ad6a129493280acd99496ab637708a222a258b3a
Prince <princekumaraman7654@gmail.com>
2025-05-24 17:47:11 +0530

    added imp fields in review model

--- src/main/java/org/example/models/Review.java
+++ src/main/java/org/example/models/Review.java
@@ -1,13 +1,31 @@
 package org.example.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "bookingReview") //custom name for the table
 public class Review {
 
     @Id  // makes the field as primary key in our DB table
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB auto increments the ID
     Long id;
 
+    @Column(nullable = false)
+    String Content;
 
+    double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // decides the format of date stored
+    @CreatedDate // tells the spring to add value when created
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate // tells the spring to add value when updated
+    Date updatedAt;
 }
